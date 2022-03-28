f13b8acd7228f78fc3b54da46a487b0eb43fc867
Boengiu <darius.boengiu@capgemini.com>
2022-03-28 11:04:49 +0300

    Added metohd for reading headers

--- src/main/java/com/capgemini/springbootcalculator/controllers/OperationController.java
+++ src/main/java/com/capgemini/springbootcalculator/controllers/OperationController.java
@@ -1,10 +1,12 @@
 package com.capgemini.springbootcalculator.controllers;
 
 import com.capgemini.springbootcalculator.services.CalculatorService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.tinylog.Logger;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/operation")
@@ -22,7 +24,17 @@ public class OperationController {
             @RequestParam String firstNumber,
             @RequestParam String secondNumber
     ) {
-        String[] parameters  = {operator, firstNumber, secondNumber};
+        String[] parameters = {operator, firstNumber, secondNumber};
         return calculatorService.executeOperation(parameters);
+    }
+
+    @GetMapping("/listHeaders")
+    public ResponseEntity<String> listAllHeaders(@RequestHeader Map<String, String> headers) {
+        headers.forEach((key, value) -> {
+            Logger.info(String.format("Header '%s' = %s", key, value));
+        });
+
+        return new ResponseEntity<String>(
+                String.format("Listed %d headers", headers.size()), HttpStatus.OK);
     }
 }
